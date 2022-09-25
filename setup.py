d62cea67789cd90ae24a812ad840b806bacf3468
baa-god <780810441@qq.com>
2022-09-26 00:48:45 +0800

    发布 v0.0.1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pylazy",
+    version="0.0.1",
+    author="baa-god",
+    author_email="780810441@qq.com",
+    description="pylazy 是一个 python 对象的懒加载装饰器，类似于 `@property` 但只会调用一次。",
+)
