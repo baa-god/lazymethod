2aa5a1dc860013b1bd834e866ef827275f935119
baa-god <780810441@qq.com>
2022-09-26 02:15:33 +0800

    发布 v0.0.2

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="lazymethod",
-    version="0.0.1",
-    author="baa-god",
-    author_email="780810441@qq.com",
-    description="pylazy 是一个 python 对象的懒加载装饰器，类似于 `@property` 但只会调用一次。",
-)
